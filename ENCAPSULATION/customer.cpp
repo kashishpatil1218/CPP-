e3125a04581f9353717ad4699af2b36c16647371
Kashish patil <156797313+kashishpatil1218@users.noreply.github.com>
2024-04-04 10:42:49 +0530

    Add files via upload

---
+++ ENCAPSULATION/customer.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+	//- cust_id
+	//- cust_name
+	//- cust_age
+	//- cust_city
+	//- cust_mobile_number
+	//- cust_simcard_validity (in years)
+	//- cust_telecom_brand_name (like Jio, Airtel, Vi, etc.)
+	
+	class cust
+	{
+		public :
+		int id;
+		char name[20];
+		int age;
+		char city[20];
+		double m_num;
+		int sim_val;
+		char sim_brand[20];
+		
+		void input()
+		{
+			cout << "Enter ID :";
+			cin >> id;
+			cout << "Enter name :";
+			cin >> name;
+			cout << "Enter age :";
+			cin >> age;
+			cout << "Enter city :";
+			cin >> city;	
+			cout << "Enter m_num :";
+			cin >> m_num;	
+			cout << "Enter sim_val :";
+			cin >> sim_val;
+			cout << "Enter sim_brand :";
+			cin >> sim_brand;
+			
+			cout << endl;
+		}	
+		
+		void output()
+		{
+			cout << "----------------------" << endl;
+			cout << "Customer ID :" << id << endl;
+			cout << "Customer name :" << name << endl;
+			cout << "Customer age :" << age << endl;
+			cout << "Customer city :" << city << endl;
+			cout << "Customer m_num :" << m_num << endl;
+			cout << "Customer sim_val :" << sim_val << endl;
+			cout << "Customer sim_brand :" << sim_brand << endl << endl;;
+			
+		}
+	};
+int main()
+{
+	cust c1,c2,c3,c4,c5;
+	
+	c1.input();
+	c2.input();
+	c3.input();
+	c4.input();
+	c5.input();
+	
+	c1.output();
+	c2.output();
+	c3.output();
+	c4.output();
+	c5.output();
+	return 0;
+}
---
+++ ENCAPSULATION/student.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+	//- stu_id
+	//- stu_name
+	//- stu_age
+	//- stu_course
+	//- stu_city
+	//- stu_email
+	//- stu_college
+	
+	class stu
+	{
+	    public:
+	    	
+		int id;
+		char name[20];
+		int age;
+		char course[20];
+		char city[20];
+		char mail[30];
+		char c_name[50];
+		
+		void input()
+		{
+			cout << "Enter ID :";
+			cin >> id;
+			cout << "Enter name :";
+			cin >> name;
+			cout << "Enter age :";
+			cin >> age;
+			cout << "Enter course :";
+			cin >> course;
+			cout << "Enter city :";
+			cin >> city;
+			cout << "Enter mail :";
+			cin >> mail;
+			cout << "Enter c_name :";
+			cin >> c_name;
+			
+			cout << endl;
+		}
+		
+		void output()
+		{
+			cout << "---------------------------" << endl;
+			cout << "student id :" << id << endl;
+			cout << "student name :" << name << endl;
+			cout << "student age :" << age << endl;
+			cout << "student course :" << course << endl;
+			cout << "student city :" << city << endl;
+			cout << "student mail :" << mail << endl;
+			cout << "student c_name :" << c_name << endl << endl;
+		}
+	};
+	
+int main()
+{
+	stu s1,s2,s3,s4,s5;
+	
+	s1.input();
+	s2.input();
+	s3.input();
+	s4.input();
+	s5.input();
+	
+	s1.output();
+	s2.output();
+	s3.output();
+	s4.output();
+	s5.output();
+	
+	return 0;
+}
